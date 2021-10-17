d4ae98888d5563e931979507b570db02ac0d8376
Jan Seelbach <jan.seelbach97@gmail.com>
2021-10-17 18:33:37 +0200

    add navbar from example
    https://www.freecodecamp.org/news/how-to-build-a-single-page-application-using-vue-js-vuex-vuetify-and-firebase-838b40721a07/

--- personal-website/src/plugins/vuetify.js
+++ personal-website/src/plugins/vuetify.js
@@ -3,5 +3,22 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+import colors from 'vuetify/lib/util/colors'
+
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.purple,
+        secondary: colors.grey.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
+    },
+  },
+})
+
+export default vuetify;
