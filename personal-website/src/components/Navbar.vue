a0c6947a4a72acbfc6d9662aae8015a1a235fe1b
Jan Seelbach <jan.seelbach97@gmail.com>
2021-10-31 22:02:22 +0100

    added slider

--- personal-website/src/components/Navbar.vue
+++ personal-website/src/components/Navbar.vue
@@ -26,9 +26,6 @@
       <v-spacer class="hidden-md-and-up"></v-spacer>
       <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
       <v-btn text class="hidden-sm-and-down">Menu</v-btn>
-      <v-spacer class="hidden-sm-and-down"></v-spacer>
-      <v-btn text class="hidden-sm-and-down">SIGN IN</v-btn>
-      <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
     </v-app-bar>
   </span>
 </template>
--- personal-website/src/components/Projects.vue
+++ personal-website/src/components/Projects.vue
@@ -1,140 +1,98 @@
 <template>
-  <v-container grid-list-lg>
-    <v-row row>
-      <v-col xs12 class="text-xs-center display-1 font-weight-black my-5"
-        >Projects</v-col
-      >
-    </v-row>
-    <v-row row wrap class="meal-plans">
-      <v-col xs12 sm12 md4>
+  <v-container fluid>
+    <v-container class="inner-container">
+      <v-row>
+        <v-col xs12 class="text-xs-center display-1 font-weight-black my-5"
+          >Projects</v-col
+        >
+      </v-row>
+      <v-col xs12 sm12 md12>
         <v-card>
-          <v-responsive>
-            <v-img src="http://source.unsplash.com/hjCA3ecCXAQ" height="500px">
-              <v-container fill-height fluid>
-                <v-row fill-height>
-                  <v-col xs12 align-end flexbox>
-                    <span class="headline white--text">KETO</span>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-img>
-          </v-responsive>
-
+          <v-card-title>OnlineIDE</v-card-title>
           <v-card-text>
-            <div>
-              <h3 class="headline mb-0">Keto</h3>
-              <div>
-                The Keto diet is a high-fat, adequate-protein, low-carbohydrate
-                diet. The diet forces the body to burn fats rather than
-                carbohydrates by putting the body into ketosis.
-              </div>
-            </div>
+            The Keto diet is a high-fat, adequate-protein, low-carbohydrate
+            diet. The diet forces the body to burn fats rather than
+            carbohydrates by putting the body into ketosis. The Keto diet is a
+            high-fat, adequate-protein, low-carbohydrate diet. The diet forces
+            the body to burn fats rather than carbohydrates by putting the body
+            into ketosis. The Keto diet is a high-fat, adequate-protein,
+            low-carbohydrate diet. The diet forces the body to burn fats rather
+            than carbohydrates by putting the body into ketosis. The Keto diet
+            is a high-fat, adequate-protein, low-carbohydrate diet. The diet
+            forces the body to burn fats rather than carbohydrates by putting
+            the body into ketosis. The Keto diet is a high-fat,
+            adequate-protein, low-carbohydrate diet. The diet forces the body to
+            burn fats rather than carbohydrates by putting the body into
+            ketosis.
           </v-card-text>
+        </v-card>
+      </v-col>
 
-          <v-card-actions v-if="['menu'].includes($route.name)">
-            <v-btn
-              outline
-              block
-              color="green"
-              data-cy="plansKetoBtn"
-              @click="showRecipes('keto')"
-            >
-              Select This Plan</v-btn
-            >
-          </v-card-actions>
+      <v-col md4>
+        <v-card>
+          <v-card-title>myKFZ</v-card-title>
+          <v-carousel
+            height="auto"
+            cycle
+            hide-delimiters
+            show-arrows-on-hover
+            eager
+          >
+            <v-carousel-item v-for="(slide, i) in myKFZslides" :key="i" eager>
+              <v-img
+                height="auto"
+                class="mx-auto"
+                contain
+                :src="slide.src"
+                aspect-ratio="1.77"
+              ></v-img></v-carousel-item
+          ></v-carousel>
         </v-card>
       </v-col>
 
       <v-col xs12 sm12 md4>
         <v-card>
-          <v-responsive>
-            <v-img src="http://source.unsplash.com/6S27S6pZ6o0" height="500px">
-              <v-container fill-height fluid>
-                <v-row fill-height>
-                  <v-col xs12 align-end flexbox>
-                    <span class="headline white--text">PALEO</span>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-img>
-          </v-responsive>
-
+          <v-card-title>Secret Santa</v-card-title>
           <v-card-text>
-            <div>
-              <h3 class="headline mb-0">Paleo</h3>
-              <div>
-                The Paleo diet requires the predominant consumption of foods
-                presumed to have been the only foods available to or consumed by
-                humans during the Paleolithic era.
-              </div>
-            </div>
+            The Keto diet is a high-fat, adequate-protein, low-carbohydrate
+            diet. The diet forces the body to burn fats rather than
+            carbohydrates by putting the body into ketosis.
           </v-card-text>
-
-          <v-card-actions v-if="['menu'].includes($route.name)">
-            <v-btn
-              outline
-              block
-              color="green"
-              data-cy="plansPaleoBtn"
-              @click="showRecipes('paleo')"
-            >
-              Select This Plan</v-btn
-            >
-          </v-card-actions>
         </v-card>
       </v-col>
-
-      <v-col xs12 sm12 md4>
-        <v-card>
-          <v-responsive>
-            <v-img src="http://source.unsplash.com/1SPu0KT-Ejg" height="500px">
-              <v-container fill-height fluid>
-                <v-row fill-height>
-                  <v-col xs12 align-end flexbox>
-                    <span class="headline white--text">VEGAN</span>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-img>
-          </v-responsive>
-
-          <v-card-text>
-            <div>
-              <h3 class="headline mb-0">Vegan</h3>
-              <div>
-                The vegan diet abstains from the use of animal products. The
-                vegan diet does not consume meat, diary products, eggs or any
-                all other animal-derived ingredients.
-              </div>
-            </div>
-          </v-card-text>
-
-          <v-card-actions v-if="['menu'].includes($route.name)">
-            <v-btn
-              outline
-              block
-              color="green"
-              data-cy="plansVeganBtn"
-              @click="showRecipes('vegan')"
-            >
-              Select This Plan</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+    </v-container></v-container
+  >
 </template>
 
 <script>
 export default {
   name: "Projects",
-  methods: {
-    showRecipes(plan) {
-      this.$store.dispatch("getRecipes", plan);
-    },
+  data() {
+    return {
+      myKFZslides: [
+        {
+          src: require("../assets/myKFZ/landing-page.gif"),
+        },
+        {
+          src: require("../assets/myKFZ/dashboard.png"),
+        },
+        {
+          src: require("../assets/myKFZ/add-vehicle.png"),
+        },
+        {
+          src: require("../assets/myKFZ/plate-reservation.png"),
+        },
+        {
+          src: require("../assets/myKFZ/plate-list.png"),
+        },
+        {
+          src: require("../assets/myKFZ/user-verification.png"),
+        },
+        {
+          src: require("../assets/myKFZ/plate-reservation.png"),
+        },
+      ],
+    };
   },
 };
 </script>
-
-<style scoped></style>
