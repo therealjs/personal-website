5ccce406ab66586724102710c004035c28c3052a
Jan Seelbach <jan.seelbach97@gmail.com>
2021-11-01 12:09:32 +0100

    add onlineIDE pictures and add project descriptions

--- personal-website/src/components/Projects.vue
+++ personal-website/src/components/Projects.vue
@@ -7,61 +7,113 @@
         >
       </v-row>
       <v-col xs12 sm12 md12>
-        <v-card>
-          <v-card-title>OnlineIDE</v-card-title>
-          <v-card-text>
-            The Keto diet is a high-fat, adequate-protein, low-carbohydrate
-            diet. The diet forces the body to burn fats rather than
-            carbohydrates by putting the body into ketosis. The Keto diet is a
-            high-fat, adequate-protein, low-carbohydrate diet. The diet forces
-            the body to burn fats rather than carbohydrates by putting the body
-            into ketosis. The Keto diet is a high-fat, adequate-protein,
-            low-carbohydrate diet. The diet forces the body to burn fats rather
-            than carbohydrates by putting the body into ketosis. The Keto diet
-            is a high-fat, adequate-protein, low-carbohydrate diet. The diet
-            forces the body to burn fats rather than carbohydrates by putting
-            the body into ketosis. The Keto diet is a high-fat,
-            adequate-protein, low-carbohydrate diet. The diet forces the body to
-            burn fats rather than carbohydrates by putting the body into
-            ketosis.
-          </v-card-text>
-        </v-card>
+        <v-row justify="center">
+          <v-card max-height="800px" max-width="800px" class="my-5">
+            <v-card-title>OnlineIDE</v-card-title>
+            <v-divider></v-divider>
+            <v-carousel
+              height="auto"
+              cycle
+              hide-delimiters
+              show-arrows-on-hover
+              eager
+            >
+              <v-carousel-item
+                v-for="(slide, i) in onlineIDEslides"
+                :key="i"
+                eager
+              >
+                <v-img
+                  height="auto"
+                  class="mx-auto"
+                  contain
+                  :src="slide.src"
+                  aspect-ratio="1.77"
+                ></v-img></v-carousel-item
+            ></v-carousel>
+            <v-divider></v-divider>
+            <v-card-text
+              >Developed at university as part of "Advanced Topics of Software
+              Engineering", the onlineIDE enables TUM students to manage and
+              share programming projects with fellow students. Note that this
+              project was developed primarily to work with different
+              state-of-the-art technologies and gain experience with a
+              microservices architecture, thus there is definitely a lot of room
+              for improvement at the UI!</v-card-text
+            >
+          </v-card>
+        </v-row>
       </v-col>
 
-      <v-col md4>
-        <v-card>
-          <v-card-title>myKFZ</v-card-title>
-          <v-carousel
-            height="auto"
-            cycle
-            hide-delimiters
-            show-arrows-on-hover
-            eager
-          >
-            <v-carousel-item v-for="(slide, i) in myKFZslides" :key="i" eager>
-              <v-img
-                height="auto"
-                class="mx-auto"
-                contain
-                :src="slide.src"
-                aspect-ratio="1.77"
-              ></v-img></v-carousel-item
-          ></v-carousel>
-        </v-card>
+      <v-col xs12 sm12 md12>
+        <v-row justify="center">
+          <v-card max-height="800px" max-width="800px" class="my-5">
+            <v-card-title>myKFZ</v-card-title>
+            <v-divider></v-divider>
+            <v-carousel
+              height="auto"
+              cycle
+              hide-delimiters
+              show-arrows-on-hover
+              eager
+            >
+              <v-carousel-item v-for="(slide, i) in myKFZslides" :key="i" eager>
+                <v-img
+                  height="auto"
+                  class="mx-auto"
+                  contain
+                  :src="slide.src"
+                  aspect-ratio="1.77"
+                ></v-img></v-carousel-item
+            ></v-carousel>
+            <v-divider></v-divider>
+            <v-card-text
+              >myKFZ is a platform that enables its users to register and
+              deregister vehicles from the comfort of their home! Users can also
+              reserve license plates. District authorities can accept and reject
+              processes in their own dashboard.</v-card-text
+            >
+          </v-card>
+        </v-row>
       </v-col>
 
-      <v-col xs12 sm12 md4>
-        <v-card>
-          <v-card-title>Secret Santa</v-card-title>
-          <v-card-text>
-            The Keto diet is a high-fat, adequate-protein, low-carbohydrate
-            diet. The diet forces the body to burn fats rather than
-            carbohydrates by putting the body into ketosis.
-          </v-card-text>
-        </v-card>
+      <v-col xs12 sm12 md12>
+        <v-row justify="center">
+          <v-card max-height="800px" max-width="800px" class="my-5">
+            <v-card-title>Secret Santa</v-card-title>
+            <v-divider></v-divider>
+            <v-carousel
+              height="auto"
+              cycle
+              hide-delimiters
+              show-arrows-on-hover
+              eager
+            >
+              <v-carousel-item
+                v-for="(slide, i) in secretSantaSlides"
+                :key="i"
+                eager
+              >
+                <v-img
+                  height="auto"
+                  class="mx-auto"
+                  contain
+                  :src="slide.src"
+                  aspect-ratio="1.77"
+                ></v-img></v-carousel-item
+            ></v-carousel>
+            <v-divider></v-divider>
+            <v-card-text
+              >Secret Santa will allow its users to organise secret santa in a
+              minute. Enter the email addresses of all participants, a time and
+              place, the maximum value of a gift, and few kind words to get the
+              organisational things done!
+            </v-card-text>
+          </v-card>
+        </v-row>
       </v-col>
-    </v-container></v-container
-  >
+    </v-container>
+  </v-container>
 </template>
 
 <script>
@@ -86,10 +138,23 @@ export default {
           src: require("../assets/myKFZ/plate-list.png"),
         },
         {
-          src: require("../assets/myKFZ/user-verification.png"),
+          src: require("../assets/myKFZ/district-dashboard.png"),
+        },
+      ],
+      onlineIDEslides: [
+        {
+          src: require("../assets/onlineIDE/projects.png"),
         },
         {
-          src: require("../assets/myKFZ/plate-reservation.png"),
+          src: require("../assets/onlineIDE/editor_java.png"),
+        },
+        {
+          src: require("../assets/onlineIDE/editor_c.png"),
+        },
+      ],
+      secretSantaSlides: [
+        {
+          src: require("../assets/secretsanta/coming-soon-hour-glass.png"),
         },
       ],
     };
